bbf597674b14a1e45d7fca2427cfd964a7189797
Brice <bricejlin@gmail.com>
2013-06-17 20:04:30 -0400

    add tasks controller/routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Tasks::Application.routes.draw do
+  root 'tasks#index'
+  resources :tasks
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
